0f56c1e29c649229ce34716fc0fe548380dedbae
EthanH-0915 <eharding0915@gmail.com>
2026-01-28 15:43:20 -0500

    changed migration and removed migration_lock.toml

---
+++ backend/prisma/migrations/20260128203604_updated_user/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `password` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "password",
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "emailVerified" TIMESTAMP(3),
+ADD COLUMN     "passwordHash" TEXT,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
